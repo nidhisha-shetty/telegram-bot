dc8e540e0441b35ffeb91b2ddb6eb9e8fcc0d438
Nidhisha Shetty <shetty3491@gmail.com>
2021-05-31 09:39:46 +0530

    add telegram_bot.py file

--- telegram_bot_leetcode.py
+++ telegram_bot_leetcode.py
@@ -1,7 +1,7 @@
 import requests
 import time
-messages=["Solve a Leetcode problem anytime today","It's been half day hope you have already attempted to solve a leetcode problem","Nidhisha, hope you got the solution to the leetcode problem"]
+messages=["Nidhisha, solve a leetcode problem today"]
 for message in messages:
 	base_url="https://api.telegram.org/bot1806647189:AAE2Edo2Vk804YuQqMV2cWGqm3pjdNedFfE/sendMessage?chat_id=-501831801&text=\"{}\"".format(message)
 	requests.get(base_url)
-	time.sleep(21600)
+	
